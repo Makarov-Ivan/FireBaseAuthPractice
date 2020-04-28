944dc6abe75a6b74cabb9f9b1a46dd7ac27e680a
Makarov-Ivan <makivanik@gmail.com>
2020-04-28 13:41:02 +0300

    now we fetch data form firestore and updata ul by this data

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
+const guideList = document.querySelector(".guides");
+
+//set up some gides
+const setupGuides = (data) => {
+  let html = "";
+  data.forEach((doc) => {
+    const guide = doc.data();
+    const li = `
+    <li>
+    <div class="collapsible-header grey lighten-4">${guide.title}</div>
+    <div class="collapsible-body white">
+      <span>${guide.content}</span>
+    </div>
+    </li>
+    `;
+    html += li;
+  });
+  guideList.innerHTML = html;
+};
+
+{
+  /* <li>
+<div class="collapsible-header grey lighten-4">Guide title</div>
+<div class="collapsible-body white">
+  <span>Lorem ipsum dolor sit amet.</span>
+</div>
+</li> */
+}
+
 // setup materialize components
 document.addEventListener("DOMContentLoaded", function () {
   var modals = document.querySelectorAll(".modal");
